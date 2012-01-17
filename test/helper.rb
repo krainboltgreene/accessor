e3f67e8debbd69f0883498a980fb002eb742bf41
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-01-16 23:26:53 -0600

    Adding a dummy class for my tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,14 @@
 require "minitest/pride"
 require 'minitest/autorun'
 require 'accessor'
+
+class Person
+  accessor :name, :email
+  writer :password
+  reader :encrypted_password
+
+  def encrypt_password!
+    @encrypted_password = Base64::strict_encode64(@password)
+  end
+
+end
