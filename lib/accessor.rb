3d500ff18177c2c7f14958afcb28b9a866611530
krainboltgreene <me@kurtisrainboltgreene.name>
2012-01-16 23:33:22 -0600

    Returning the ivar value if no arg given

--- lib/accessor.rb
+++ lib/accessor.rb
@@ -96,6 +96,8 @@ module Accessor
             if arg
               instance_variable_set "@#{name}".to_sym, arg
               self
+            else
+              instance_variable_get "@#{name}".to_sym
             end
           }
       end
